329f2d3a55f13fcdb0a883d31cbefdbc782dc299
salman554480 <sa307485@gmail.com>
2024-11-12 11:58:44 +0500

    Setup File Structure

---
+++ documenter/index.php
@@ -0,0 +1,15 @@
+<?php require_once('parts/top.php'); ?>
+</head>
+
+<body>
+    <?php require_once('parts/navbar.php'); ?>
+
+    <main role="main" class="container-fluid">
+        <div class="row">
+            <div class="col-md-12"></div>
+        </div>
+    </main>
+
+</body>
+
+</html>
