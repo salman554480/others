ec6a122b7ad3dfcc776525728353b1fc72a1f66a
salman554480 <sa307485@gmail.com>
2024-11-13 09:37:55 +0500

    Added File Structure in Admin and created blank.php

---
+++ documenter/portal/blank.php
@@ -0,0 +1,25 @@
+<?php require_once('parts/top.php'); ?>
+</head>
+
+<body class="app sidebar-mini">
+    <!-- Navbar-->
+    <?php require_once('parts/navbar.php'); ?>
+    <!-- Sidebar menu-->
+    <div class="app-sidebar__overlay" data-toggle="sidebar"></div>
+    <?php require_once('parts/sidebar.php'); ?>
+    <main class="app-content">
+        <div class="app-title">
+            <div>
+                <h1><i class="bi bi-speedometer"></i> Blank Page</h1>
+                <p>Start a beautiful journey here</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="tile">
+                    <div class="tile-body">Create a beautiful dashboard</div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <?php require_once('parts/footer.php'); ?>
