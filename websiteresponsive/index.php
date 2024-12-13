adb0f3ecaec10446cd05d305fd343ea253d92e01
salman554480 <sa307485@gmail.com>
2024-12-13 13:07:52 +0500

    updated website responsiveness design

--- websiteresponsive/index.php
+++ websiteresponsive/index.php
@@ -20,15 +20,10 @@ $meta_keywords =  $row_page['meta_keywords'];
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <div class="ad-area my-4">
-                    <?php if ($ad_code_one == "") {
-						echo "<img class='w-100' src='https://dummyimage.com/1400x150/f7f7f7/000000&text=++++++++++++++++Advertisement+++++++++++++'>";
-					} {
-						echo $ad_code_one;
-					} ?>
-                </div>
+
                 <div class="row bg-white p-3 custom-shadow mb-3 searhbox">
                     <div class="col-md-12 mb-3">
+                        <h1 class="text-center main-title">Website Responsiveness Checker </h1>
                         <form id="urlForm">
                             <input type="text" id="websiteUrl"
                                 placeholder="Enter Website URL: e.g., https://example.com" required>
@@ -37,36 +32,43 @@ $meta_keywords =  $row_page['meta_keywords'];
                         <h4 class="text-center py-4" id="websiteName" style="display:none;"></h4>
                     </div>
                 </div>
+                <div class="ad-area my-4">
+                    <?php if ($ad_code_one == "") {
+                        echo "<img class='w-100' src='https://dummyimage.com/1400x150/f7f7f7/000000&text=++++++++++++++++Advertisement+++++++++++++'>";
+                    } {
+                        echo $ad_code_one;
+                    } ?>
+                </div>
                 <div class="row bg-white p-3 custom-shadow">
                     <div class="col-md-12">
                         <?php
 
-						require_once('admin/parts/db.php');
-						$select_category = "SELECT * FROM category  ORDER BY category_id ASC";
-						$run_category = mysqli_query($conn, $select_category);
-						while ($row_category = mysqli_fetch_array($run_category)) {
+                        require_once('admin/parts/db.php');
+                        $select_category = "SELECT * FROM category  ORDER BY category_id ASC";
+                        $run_category = mysqli_query($conn, $select_category);
+                        while ($row_category = mysqli_fetch_array($run_category)) {
 
-							$category_id = $row_category['category_id'];
-							$category_name = $row_category['category_name'];
-							$category_url = $row_category['category_url'];
+                            $category_id = $row_category['category_id'];
+                            $category_name = $row_category['category_name'];
+                            $category_url = $row_category['category_url'];
 
-						?>
+                        ?>
                         <h5 class="mt-5"><?php echo $category_name; ?></h5>
 
                         <div class="row">
                             <?php
 
-								$select_screen = "SELECT * FROM screen WHERE category_id='$category_id' ORDER BY screen_id DESC ";
-								$run_screen = mysqli_query($conn, $select_screen);
-								while ($row_screen = mysqli_fetch_array($run_screen)) {
+                                $select_screen = "SELECT * FROM screen WHERE category_id='$category_id' ORDER BY screen_id DESC ";
+                                $run_screen = mysqli_query($conn, $select_screen);
+                                while ($row_screen = mysqli_fetch_array($run_screen)) {
 
-									$screen_id = $row_screen['screen_id'];
-									$screen_name = $row_screen['screen_name'];
-									$screen_width = $row_screen['screen_width'];
-									$screen_height = $row_screen['screen_height'];
-									$screen_image = $row_screen['screen_image'];
+                                    $screen_id = $row_screen['screen_id'];
+                                    $screen_name = $row_screen['screen_name'];
+                                    $screen_width = $row_screen['screen_width'];
+                                    $screen_height = $row_screen['screen_height'];
+                                    $screen_image = $row_screen['screen_image'];
 
-								?>
+                                ?>
                             <div title="<?php echo $screen_width; ?> x <?php echo $screen_height; ?>"
                                 data-toggle="tooltip"
                                 onclick="testResponsive(<?php echo $screen_width; ?>, <?php echo $screen_height; ?>)"
@@ -111,10 +113,10 @@ $meta_keywords =  $row_page['meta_keywords'];
 
         <div class="ad-area my-4">
             <?php if ($ad_code_one == "") {
-				echo "<img class='w-100' src='https://dummyimage.com/1400x150/a7a7a7/000000&text=++++++++++++++++Advertisement+++++++++++++'>";
-			} {
-				echo $ad_code_one;
-			} ?>
+                echo "<img class='w-100' src='https://dummyimage.com/1400x150/a7a7a7/000000&text=++++++++++++++++Advertisement+++++++++++++'>";
+            } {
+                echo $ad_code_one;
+            } ?>
         </div>
     </div>
     <div class="container bg-white py-5">
@@ -122,13 +124,13 @@ $meta_keywords =  $row_page['meta_keywords'];
         <div id="faqAccordion" class="my-4">
             <!-- FAQ Item 1 -->
             <?php
-			$select_faq = "SELECT * FROM faq";
-			$run_faq =  mysqli_query($conn, $select_faq);
-			while ($row_faq =  mysqli_fetch_array($run_faq)) {
-				$faq_id =  $row_faq['faq_id'];
-				$faq_question =  $row_faq['faq_question'];
-				$faq_answer =  $row_faq['faq_answer'];
-			?>
+            $select_faq = "SELECT * FROM faq";
+            $run_faq =  mysqli_query($conn, $select_faq);
+            while ($row_faq =  mysqli_fetch_array($run_faq)) {
+                $faq_id =  $row_faq['faq_id'];
+                $faq_question =  $row_faq['faq_question'];
+                $faq_answer =  $row_faq['faq_answer'];
+            ?>
             <div class="card">
                 <div class="card-header" id="headingOne">
                     <h5 class="mb-0">
@@ -158,28 +160,28 @@ $meta_keywords =  $row_page['meta_keywords'];
                 <!-- Blog Post 1 -->
                 <?php
 
-				require_once('admin/parts/db.php');
-				$select = "SELECT * FROM post WHERE post_status='publish' ORDER BY post_id DESC LIMIT 3";
-				$run = mysqli_query($conn, $select);
-				while ($row = mysqli_fetch_array($run)) {
+                require_once('admin/parts/db.php');
+                $select = "SELECT * FROM post WHERE post_status='publish' ORDER BY post_id DESC LIMIT 3";
+                $run = mysqli_query($conn, $select);
+                while ($row = mysqli_fetch_array($run)) {
 
-					$post_id = $row['post_id'];
-					$post_title = $row['post_title'];
-					$post_url = $row['post_url'];
+                    $post_id = $row['post_id'];
+                    $post_title = $row['post_title'];
+                    $post_url = $row['post_url'];
 
-					$post_thumbnail = $row['post_thumbnail'];
-					$post_content = $row['post_content'];
+                    $post_thumbnail = $row['post_thumbnail'];
+                    $post_content = $row['post_content'];
 
-					// Remove HTML tags
-					$plain_text = strip_tags($post_content);
+                    // Remove HTML tags
+                    $plain_text = strip_tags($post_content);
 
-					// Decode HTML entities to their corresponding characters
-					$plain_text = html_entity_decode($plain_text);
+                    // Decode HTML entities to their corresponding characters
+                    $plain_text = html_entity_decode($plain_text);
 
-					// Optionally, remove extra whitespace
-					$plain_text = trim(preg_replace('/\s+/', ' ', $plain_text));
+                    // Optionally, remove extra whitespace
+                    $plain_text = trim(preg_replace('/\s+/', ' ', $plain_text));
 
-				?>
+                ?>
                 <div class="col-md-4 mb-4">
                     <div class="card blog-card">
                         <a href="post_details.php?post_url=<?php echo $post_url; ?>">
@@ -190,8 +192,8 @@ $meta_keywords =  $row_page['meta_keywords'];
                             <a href="post_details.php?post_url=<?php echo $post_url; ?>" <h5
                                 class="card-title"><?php echo substr($post_title, 0, 60); ?></h5>
                                 <?php if (strlen($post_title) > 50) {
-										echo "...";
-									} ?>
+                                        echo "...";
+                                    } ?>
                             </a>
                             <p class="card-text"><?php echo substr($post_content, 8, 50) ?></p>
                         </div>
